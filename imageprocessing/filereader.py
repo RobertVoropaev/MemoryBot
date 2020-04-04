b5723f45068fad1efbcaee9e9ad9311c0270c914
RobertVoropaev <robertvoropaev11@gmail.com>
2020-04-04 17:28:06 +0300

    colorization MVP

--- imageprocessing/filereader.py
+++ imageprocessing/filereader.py
@@ -1,9 +1,3 @@
-import Algorithmia
 
-apiKey = 'simIVvEvW9njMmOIYwAtYFdxtFe1'
-client = Algorithmia.client(apiKey)
+print(get_color_name("data/dir/1234.png"))
 
-t800File = client.file("data://.algo/deeplearning/ColorfulImageColorization/temp/a006b777f7b74df370efaa0af67ff51f_origin.png").getBytes()
-print(t800File)
-
-with open(Э)
