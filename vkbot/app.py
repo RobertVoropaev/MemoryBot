42080ebd8baaa4ea81e4185bfcf9d79b1d964712
Mikhail Kuznetsov <61391551+kuznetsov-m@users.noreply.github.com>
2020-04-05 03:38:44 +0300

    add config.json

--- vkbot/app.py
+++ vkbot/app.py
@@ -9,11 +9,23 @@ from VkBot import VkBot
 def write_msg(user_id, message):
     vk.method('messages.send', {'user_id': user_id, 'message': message, 'random_id': random.randint(0, 2048)})
     
+import json
+import sys
 
-token = "206a6ee2c7c2c10793de804210350d8b9dc055f467f5d1a8a231e554ef9b9326be70216ab5e6a8c084f3a"
-# KM
-# token = "52def8326ae174c1963079e5d9c9dda0fcf1f0b77a9d999abe2382fa8cc30dd5d0d4b6f5cc823ae454471"
+CONFIG_FILE = 'config.json'
+config = None
 
+with open(CONFIG_FILE) as json_file:
+    config = json.load(json_file)
+
+keys = {'KEY'}
+
+for key in keys:
+    if key not in config:
+        print('Error: incorrect %s file: %s key is missing' % (CONFIG_FILE, key))
+        sys.exit()
+
+token = config['KEY']
 
 vk = vk_api.VkApi(token=token)
 
